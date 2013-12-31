d0d309b578ff334d52347032ae29989cffbe04d5
Zaltman Bleros <zaltmanbleros@gmail.com>
2013-12-31 08:16:27 -0500

    Functional, but not pretty.

--- src/com/bleros/zaltman/miniFlashLight/MiniFlashLight.java
+++ src/com/bleros/zaltman/miniFlashLight/MiniFlashLight.java
@@ -1,17 +1,101 @@
 package com.bleros.zaltman.miniFlashLight;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.view.View;
+import android.widget.ToggleButton;
+import com.sony.smallapp.SmallAppWindow;
+import com.sony.smallapp.SmallApplication;
 
-public class MiniFlashLight extends Activity
+public class MiniFlashLight extends SmallApplication
 {
-    /**
-     * Called when the activity is first created.
-     */
+    private static Camera mCamera = null;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate()
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate();
+
         setContentView(R.layout.main);
+        setTitle(R.string.app_name);
+
+        SmallAppWindow.Attributes attr = getWindow().getAttributes();
+        attr.flags &= ~SmallAppWindow.Attributes.FLAG_RESIZABLE;
+        attr.flags |= SmallAppWindow.Attributes.FLAG_NO_TITLEBAR;
+        getWindow().setAttributes(attr);
+
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (toggleButton.isChecked())
+                {
+                    flashLightOn();
+                }
+                else
+                {
+                    flashLightOff();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        flashLightOff();
+    }
+
+    public void flashLightOn()
+    {
+
+        try
+        {
+            if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
+            {
+                mCamera = Camera.open();
+                Camera.Parameters p = mCamera.getParameters();
+                p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                mCamera.setParameters(p);
+                mCamera.startPreview();
+                mCamera.unlock();
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void flashLightOff()
+    {
+        try
+        {
+            if ((mCamera != null) && getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
+            {
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
