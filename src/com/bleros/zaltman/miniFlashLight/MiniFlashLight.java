166af1e6cdca00486f8e5baf3383e60f8a1ed88d
Zaltman Bleros <zaltmanbleros@gmail.com>
2013-12-31 10:57:20 -0500

    Don't restart on orientation change. Also, start with light on.

--- src/com/bleros/zaltman/miniFlashLight/MiniFlashLight.java
+++ src/com/bleros/zaltman/miniFlashLight/MiniFlashLight.java
@@ -1,7 +1,10 @@
 package com.bleros.zaltman.miniFlashLight;
 
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.View;
 import android.widget.ToggleButton;
 import com.sony.smallapp.SmallAppWindow;
@@ -9,13 +12,17 @@ import com.sony.smallapp.SmallApplication;
 
 public class MiniFlashLight extends SmallApplication
 {
-    private Camera mCamera = null;
+    private static final String TAG = MiniFlashLight.class.getSimpleName();
+    private Camera mCamera;
+    private Configuration mConfig;
 
     @Override
     public void onCreate()
     {
+        Log.d(TAG, "onCreate()");
         super.onCreate();
 
+        mConfig = new Configuration(getResources().getConfiguration());
         setContentView(R.layout.main);
         setTitle(R.string.app_name);
 
@@ -40,29 +47,35 @@ public class MiniFlashLight extends SmallApplication
                 }
             }
         });
+
+        toggleButton.setChecked(flashLightOn());
     }
 
     @Override
     public void onStart()
     {
+        Log.d(TAG, "onStart()");
         super.onStart();
     }
 
     @Override
     public void onStop()
     {
+        Log.d(TAG, "onStop()");
         super.onStop();
     }
 
     @Override
     public void onDestroy()
     {
+        Log.d(TAG, "onDestroy()");
         super.onDestroy();
         flashLightOff();
     }
 
-    public void flashLightOn()
+    public boolean flashLightOn()
     {
+        Log.d(TAG, "flashLightOn()");
         try
         {
             if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
@@ -72,17 +85,20 @@ public class MiniFlashLight extends SmallApplication
                 p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                 mCamera.setParameters(p);
                 mCamera.startPreview();
-                mCamera.unlock();
+                return true;
             }
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
+
+        return false;
     }
 
     public void flashLightOff()
     {
+        Log.d(TAG, "flashLightOff()");
         try
         {
             if ((mCamera != null) && getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
@@ -96,5 +112,22 @@ public class MiniFlashLight extends SmallApplication
         {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected boolean onSmallAppConfigurationChanged(Configuration newConfig)
+    {
+        Log.d(TAG, "onSmallAppConfigurationChanged()");
+        int diff = newConfig.diff(mConfig);
+        mConfig = new Configuration(getResources().getConfiguration());
+
+        // Avoid application from restarting when orientation changed
+        if ((diff & ActivityInfo.CONFIG_ORIENTATION) != 0)
+        {
+            Log.d(TAG, "onSmallAppConfigurationChanged() - change of orientation detected...");
+            return true;
+        }
+
+        return super.onSmallAppConfigurationChanged(newConfig);
     }
 }
